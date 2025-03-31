2eec8609e98de4292917a1e67aa721c35ff1129d
Angela Carvalho <angeladanda@hotmail.com>
2025-03-31 20:08:21 -0300

    update

--- desafio01/dist/script.js
+++ desafio01/dist/script.js
@@ -1,3 +1,11 @@
-valor_btc = prompt("Digite o valor em Bitcoin para conversão:")
-valor_rl = 477083.89
-alert("O valor da conversão de BTC em Reais é : R$" + valor_btc*valor_rl)
+
+//Alertas de Saudação
+alert("Olá, Seja Bem vindo! Esse é meu primeiro projeto do Imersão Dev!")
+alert("Click em *Bora Converter* para começar!")
+
+function conversor(){
+    alert("Em instante você começará a conversão!");
+    valor_btc = prompt("Digite o valor em Bitcoin para conversão:")
+    valor_rl = 477083.89
+    alert("O valor da conversão de BTC em Reais é : R$" + valor_btc*valor_rl)
+}
