5af7d6303414729e458c73b44d2af2f8e7dafa09
Angela Carvalho <angeladanda@hotmail.com>
2025-04-01 15:05:33 -0300

    desafio 2

---
+++ desafio02/README.md
@@ -0,0 +1,6 @@
+# Aula02 - Angela Carvalho - Imersão DEV
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/AngelaCCarvalho/pen/KwKbzbw](https://codepen.io/AngelaCCarvalho/pen/KwKbzbw).
+
---
+++ desafio02/dist/script.js
@@ -0,0 +1,35 @@
+idade = prompt("Qual a sua idade?");
+let podeJogar = false;
+if(idade>=18){
+  alert("Você pode prosseguir e jogar Jokenpô");
+  podeJogar = true;
+ }else{
+  alert("Voê não pode processeguir!")
+ }
+function jogar(){
+  if(!podeJogar){
+    alert("Não pode processeguir, você é menor de idade!");
+    return;
+  }
+ escolhaJogador = prompt("Faça sua escolha: [1] Pedra, [2] Papel ou [3] Tesoura?")
+ escolhaComputador = Math.floor(Math.random() * 3) + 1;
+  if(escolhaJogador > 3){
+     alert("Valor Inválido! Tente Novamente!")
+    return;
+   }
+  alert("Escolha Jogador:  " + escolhaJogador + "  " + " - Escolha do Computador:  " +           escolhaComputador)  
+   
+   if(escolhaJogador == escolhaComputador){
+     alert("Empate!")
+   }else if(
+     (escolhaJogador === 1 && escolhaComputador === 3)
+     (escolhaJogador === 2 && escolhaComputador === 1
+     (escolhaJogador === 3 && escolhaComputador === 2)
+   ){
+     alert("Jogador Venceu!")
+     }else{
+       alert("Computador Venceu!")
+     }
+  
+   
+}
