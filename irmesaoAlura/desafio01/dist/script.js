a52109f4d00c7b3993ba7e15590e8212611bfaa7
Angela Carvalho <angeladanda@hotmail.com>
2025-04-01 23:20:37 -0300

    Arrumação de formatação da moeda

--- irmesaoAlura/desafio01/dist/script.js
+++ irmesaoAlura/desafio01/dist/script.js
@@ -6,6 +6,6 @@ alert("Click em *Bora Converter* para começar!")
 function conversor(){
     alert("Em instante você começará a conversão!");
     valor_btc = prompt("Digite o valor em Bitcoin para conversão:")
-    valor_rl = 477083.89
-    alert("O valor da conversão de BTC em Reais é : R$" + valor_btc*valor_rl)
-}
+    valor_rl = 477083.89;
+    const valor = (valor_btc*valor_rl).toLocaleString('pt-Br', { style:'currency', currency:'BRL'});
+    alert("O valor da conversão de BTC em Reais é :" + valor)};
