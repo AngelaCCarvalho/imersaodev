84c4dc5d9e37dc4a030c1188fffaf10660b71e49
Angela Carvalho <angeladanda@hotmail.com>
2025-04-02 20:36:00 -0300

    Atualização d03

--- irmesaoAlura/desafio03/aula03-angela-carvalho/dist/script.js
+++ irmesaoAlura/desafio03/aula03-angela-carvalho/dist/script.js
@@ -1,14 +1,34 @@
-rodada = 1
-while(rodada <=6){
-  console.log('Rodada é:'+ rodada)
-  
-  escolhaJogador = prompt("Nível:"+rodada)
-  pisoQuebrado = Math.floor(Math.random()*6)+1;
-  
-  if(escolhaJogador == pisoQuebrado){
-    alert('Você conseguiu! Parabéns!')
-  
-    alert("Que pena, você perdeu! Tente novamente!")
-  }
-  rodada = rodada + 1
+
+document.addEventListener('DOMContentLoaded',function(){
+    
+alert("Bem vindo ao meu terceiro projeto da Imersão Dev - Alura")
+});
+
+let escolhaComputador = Math.floor(Math.random()*6) + 1;
+
+function jogar1(){
+    botao(1)
 }
+function jogar2(){
+    botao(2)    
+}
+function jogar3(){
+    botao(3) 
+}
+function jogar4(){
+    botao(4) 
+}
+function jogar5(){
+    botao(5)
+}
+function jogar6(){
+    botao(6)
+}
+
+ function botao(palpite){
+    if(palpite ==escolhaComputador){
+        alert('Parabéns você venceu!')
+    }else{
+        alert("Que pena, você errou! O número era: " + escolhaComputador)
+    }
+ }
